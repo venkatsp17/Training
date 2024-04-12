09b9e69a71b3c24aae615547e648a9020bb0405b
Venkat S P <venkatsp2002@gmail.com>
2024-04-12 13:05:01 +0530

    Day 4

---
+++ Day 4/Class&Objs/Class&Objs/Models/Employee.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Class_Objs.Models
+{
+    public class Employee
+    {
+        double salary;
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+
+        public Employee(double salary, int id, string name, DateTime dateOfBirth, string email) : this(id)
+        {
+            Id = id;
+            Name = name;
+            DateOfBirth = dateOfBirth;
+            Email = email;
+            Salary = salary;
+        }
+
+        public void PrintEmployeeDetails()
+        {
+            Console.WriteLine($"Employee Id\t:\t{Id}");
+            Console.WriteLine($"Employee name\t:\t{Name}");
+            Console.WriteLine($"Employee Date Of Birth\t:\t{DateOfBirth}");
+            Console.WriteLine($"Employee Salary\t:\tRs.{Salary}");
+            Console.WriteLine($"Employee Email\t:\t{Email}");
+        }
+
+        public Employee()
+        {
+            Id = 0;
+            Name = string.Empty;
+            DateOfBirth = DateTime.MinValue;
+            Email = string.Empty;
+            Salary = 0;
+        }
+
+        public double Salary
+        {
+            set
+            {
+                salary = value - value / 10;
+            }
+            get
+            {
+                return salary;
+            }
+        }
+        /// <summary>
+        /// Get Employee ID
+        /// </summary>
+        /// <param name="id">Employee Id as integer</param>
+        public Employee(int id) => Id = id;
+
+        internal void Work()
+        {
+            Console.WriteLine("Working...");
+        }
+
+    }
+}
---
+++ Day 4/Class&Objs/Class&Objs/Program.cs
@@ -0,0 +1,26 @@
+﻿using Class_Objs.Models;
+
+namespace Class_Objs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.Id = 1;
+            emp.Salary = 10000;
+            emp.Name = "Venkat S P";
+            Employee emp2 = new Employee(2);
+            Console.WriteLine($"Id: {emp.Id}\nName: {emp.Name}\nSalary:{emp.Salary} ");
+            Employee employee2 = new Employee
+            {
+                Name = "Somu",
+                DateOfBirth = new DateTime(2000, 3, 6),
+                Email = "somu@abccorp.com",
+                Salary = 40000
+            };
+            employee2.PrintEmployeeDetails();
+            emp.Work();
+        }
+    }
+}
