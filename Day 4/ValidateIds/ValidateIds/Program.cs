04e3a910f8fe57ad29145f08cb0865b529bae08a
Venkat S P <venkatsp2002@gmail.com>
2024-04-12 16:36:10 +0530

    Day 4

---
+++ Day 4/ValidateIds/ValidateIds/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ValidateIds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long id = long.Parse(Console.ReadLine());
+            int c = 1;
+            long rev=0;
+            while(id > 0)
+            {
+                int digit = (int)(id % 10);
+                if(c%2==0)
+                {
+                    digit *= 2;
+                    if((digit.ToString()).Length == 2)
+                    {
+                        digit = digit / 10 + digit % 10;
+                    }
+                }
+                rev += digit;
+                id /= 10;
+                c++;
+            }
+            if (rev % 10 == 0)
+            {
+                Console.WriteLine("Valid ID");
+            }
+            //Console.WriteLine(rev);
+        }
+    }
+}
