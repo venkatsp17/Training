dccf55f1f1bfe08e6b66d4433b9ffd97ab2f1434
Venkat S P <venkatsp2002@gmail.com>
2024-04-12 16:05:38 +0530

    Day 2 and Day 4

---
+++ Day 4/DoctorClass/DoctorClass/Models/Doctor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoctorClass.Models
+{
+    class Doctor
+    {
+        int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Experience { get; set; }
+        public string Qualification { get; set; }
+        public string Specialization { get; set; }
+
+        public Doctor(int id) => Id = id;
+
+        public void PrintDetails()
+        {
+            Console.WriteLine($"Doctor Id\t:\t {Id}");
+            Console.WriteLine($"Doctor Name\t:\t {Name}");
+            Console.WriteLine($"Doctor Age\t:\t {Age}");
+            Console.WriteLine($"Doctor Experience\t:\t {Experience}");
+            Console.WriteLine($"Doctor Qualification\t:\t {Qualification}");
+            Console.WriteLine($"Doctor Specialization\t:\t {Specialization}");
+        }
+    }
+}
---
+++ Day 4/DoctorClass/DoctorClass/Program.cs
@@ -0,0 +1,63 @@
+﻿using DoctorClass.Models;
+
+namespace DoctorClass
+{
+    public class Program
+    {
+        Doctor CreateDoctorDetails(int id)
+        {
+            Console.WriteLine($"Doctor {id-100} Detail\n");
+            Doctor doc = new Doctor(id);
+            Console.WriteLine("Enter Doctor Name:");
+            doc.Name = Console.ReadLine();
+            int age;
+            Console.WriteLine("Enter Doctor Age:");
+            while (!int.TryParse(Console.ReadLine(), out age))
+            {
+                Console.WriteLine("Invalid entry. Please try again.");
+            }
+            int exp;
+            Console.WriteLine("Enter Doctor Experience:");
+            while (!int.TryParse(Console.ReadLine(), out exp))
+            {
+                Console.WriteLine("Invalid entry. Please try again.");
+            }
+            doc.Age = age;
+            doc.Experience = exp;
+            Console.WriteLine("Enter Doctor Qualification:");
+            doc.Qualification = Console.ReadLine();
+            Console.WriteLine("Enter Doctor Specialization:");
+            doc.Specialization = Console.ReadLine();
+
+            return doc;
+        }
+        static void Main(string[] args)
+        {
+            Program pro = new Program();
+            Doctor[] docs = new Doctor[3];
+            for (int i = 0; i < docs.Length; i++)
+            {
+                docs[i] = pro.CreateDoctorDetails(101 + i);
+                Console.WriteLine("\n\n\n");
+            }
+            Console.WriteLine("\n\n\nDoctor Details:");
+            for (int i = 0; i < docs.Length; i++)
+            {
+                docs[i].PrintDetails();
+                Console.WriteLine("\n");
+            }
+            Console.WriteLine("\n\n\n");
+            Console.WriteLine("\n\n\nFind Doctor by Specialization");
+            Console.WriteLine("Enter the Specialization to proceed further:");
+            string spcl = Console.ReadLine();
+            for (int i = 0;i < docs.Length;i++)
+            {
+                if (docs[i].Specialization == spcl)
+                {
+                    docs[i].PrintDetails();
+                    break;
+                }
+            }
+        }
+    }
+}
