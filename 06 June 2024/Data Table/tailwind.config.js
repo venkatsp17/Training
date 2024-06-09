d8f286034f750254cb8bec794302e7314e4b3c89
Venkat S P <venkatsp2002@gmail.com>
2024-06-09 23:11:48 +0530

    June 06 Task Completed

---
+++ 06 June 2024/Data Table/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./public/index.html'],
+  theme: {
+    boxShadow: {
+      'custom-1': '0px 2px 5px 0px rgba(89, 96, 120, 0.1)',
+      'custom-2': '0px 0px 0px 1px rgba(70, 79, 96, 0.15)',
+      'custom-3': '0px 1px 1px 0px rgba(0, 0, 0, 0.1)',
+    },
+  },
+  plugins: [],
+};
